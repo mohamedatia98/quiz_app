fb89fc488751a89723e8888111cbf128e8972e8d
mohamedatia98 <mohamedatia039@gmail.com>
2023-08-02 09:40:51 +0200

    adding some statefulwidgets

---
+++ lib/questions_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
+
+  @override
+  State<QuestionsScreen> createState() {
+    return _Quiz();
+  }
+}
+
+class _Quiz extends State<QuestionsScreen> {
+  @override
+  Widget build(context) {
+    return const Text('hello');
+  }
+}
